scaling bugfix

src/ann/neural_layer.py
@@ -30,11 +30,12 @@ class Dense:
 		return output
 
 	def backward(self, grad):
-		n_s = self.input.shape[0]
-		self.dw = np.matmul(self.input.T, grad)
-		self.db = np.sum(grad, axis=0, keepdims=True)
-		return np.matmul(grad, self.W.T)
 
+		batch_size = self.input.shape[0]
+		self.dw = np.matmul(self.input.T, grad) / batch_size
+		self.db = np.sum(grad, axis=0, keepdims=True) / batch_size
+		return np.matmul(grad, self.W.T)
+		
 	def get_params(self):
 		return {"W": self.W, "b": self.b}
 