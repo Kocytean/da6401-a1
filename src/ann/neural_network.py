8c897b73853c52044d41aa6a5bf502160d53823e
Kocytean <affirmation098@gmail.com>
2026-03-07 20:20:35 +0530

    whae

--- src/ann/neural_network.py
+++ src/ann/neural_network.py
@@ -84,7 +84,8 @@ class NeuralNetwork:
 
 		grad_W_list = []
 		grad_b_list = []
-
+		if getattr(self.loss, "labels", None) is None:
+			self.loss.forward(y_pred, y_true)
 		dL = self.loss.backward()
 
 		# output layer
